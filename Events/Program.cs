22c9ebaefb65fb778440b8480dca4c15c8bae934
Khodatskaa <khodatskaalina@gmail.com>
2024-03-24 14:02:41 +0200

    Ex 2 was done

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,9 +1,33 @@
 ﻿namespace Events
 {
+    public static class StringExtensions
+    {
+        public static int WordCount(this string str)
+        {
+            string[] words = str.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+    }
+
     public class Program
     {
         static void Main()
         {
+            string text = "This is a sample sentence with several words.";
+            int wordCount = text.WordCount();
+            Console.WriteLine($"Number of words in the text: {wordCount}");
+
+            string emptyText = "";
+            int emptyWordCount = emptyText.WordCount();
+            Console.WriteLine($"Number of words in the empty text: {emptyWordCount}");
+
+            string whitespaceText = "    \t \n \r ";
+            int whitespaceWordCount = whitespaceText.WordCount();
+            Console.WriteLine($"Number of words in the whitespace text: {whitespaceWordCount}");
+
+            string singleWordText = "Hello";
+            int singleWordCount = singleWordText.WordCount();
+            Console.WriteLine($"Number of words in the single word text: {singleWordCount}");
         }
     }
 }
