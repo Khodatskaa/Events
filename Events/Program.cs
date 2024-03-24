85ccb017d0296dc7377c5f72798fdbca34ceb200
Khodatskaa <khodatskaalina@gmail.com>
2024-03-24 14:06:43 +0200

    Ex 3 was done

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace Events
 {
+    public static class StringExtensions
+    {
+        public static int LengthOfLastWord(this string str)
+        {
+            string[] words = str.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length > 0)
+            {
+                return words[words.Length - 1].Length;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
     public class Program
     {
         static void Main()
         {
+            string str = "Hello World";
+            Console.WriteLine(str.LengthOfLastWord());
         }
     }
 }
