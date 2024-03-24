fff6304c39022101bf37b4edee286cb95237f0d8
Khodatskaa <khodatskaalina@gmail.com>
2024-03-24 13:08:47 +0200

    Ex 4 was done

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,9 +1,33 @@
 ﻿namespace Events
 {
+    public static class StringExtensions
+    {
+        public static int CountVowels(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
+            int count = 0;
+            foreach (char c in input.ToLower())
+            {
+                if ("aeiou".Contains(c))
+                    count++;
+            }
+            return count;
+        }
+    }
+
     public class Program
     {
         static void Main()
         {
+            string str1 = "Hello World";
+            string str2 = "This is a test string";
+            string str3 = "";
+
+            Console.WriteLine($"Number of vowels in \"{str1}\": {str1.CountVowels()}"); 
+            Console.WriteLine($"Number of vowels in \"{str2}\": {str2.CountVowels()}"); 
+            Console.WriteLine($"Number of vowels in empty string: {str3.CountVowels()}"); 
         }
     }
 }
