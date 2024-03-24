2bd449dd690bfea1f0d23e6879bede6717975741
Khodatskaa <khodatskaalina@gmail.com>
2024-03-24 13:01:50 +0200

    Ex 2 was done

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,9 +1,30 @@
 ﻿namespace Events
 {
+    public static class IntExtensions
+    {
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool IsOdd(this int number)
+        {
+            return !number.IsEven();
+        }
+    }
+
     public class Program
     {
         static void Main()
         {
+            int num1 = 5;
+            int num2 = 10;
+
+            Console.WriteLine($"{num1} is even? {num1.IsEven()}"); 
+            Console.WriteLine($"{num1} is odd? {num1.IsOdd()}");   
+
+            Console.WriteLine($"{num2} is even? {num2.IsEven()}"); 
+            Console.WriteLine($"{num2} is odd? {num2.IsOdd()}");
         }
     }
 }
